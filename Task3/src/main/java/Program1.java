2a7c41118475d1496adbfe9895e41fb29b62a377
NguyenLam101101 <nguyenlam10112001@gmail.com>
2022-06-15 21:42:20 +0700

    nop task 4

--- Task3/src/main/java/Program1.java
+++ Task3/src/main/java/Program1.java
@@ -1,54 +1,54 @@
-//import java.io.File;
-//import java.io.IOException;
-//import org.apache.avro.Schema;
-//import org.apache.avro.file.DataFileReader;
-//import org.apache.avro.generic.GenericData;
-//import org.apache.avro.generic.GenericDatumReader;
-//import org.apache.avro.generic.GenericDatumWriter;
-//import org.apache.avro.generic.GenericRecord;
-//import org.apache.avro.file.DataFileWriter;
-//
-//public class Program1 {
-//    public static GenericRecord CreateEmployee(Schema schema, int id, String firstName, String lastName, int birthYear, double salaryRate, String position, String department){
-//        GenericRecord employee = new GenericData.Record(schema);
-//        employee.put("id", id);
-//        employee.put("firstName", firstName);
-//        employee.put("lastName", lastName);
-//        employee.put("birthYear", birthYear);
-//        employee.put("salaryRate", salaryRate);
-//        employee.put("position", position);
-//        employee.put("department", department);
-//        return employee;
-//    }
-//
-//    public static void Serialize(Schema schema, GenericRecord[] employee, File fw) throws IOException{
-//        GenericDatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<> (schema);
-//        DataFileWriter<GenericRecord> writer = new DataFileWriter<>(datumWriter);
-//        writer.create(schema, fw);
-//        for(GenericRecord i:employee){
-//            writer.append(i);
-//        }
-//        writer.close();
-//    }
-//
-//    public static void Deserialize(Schema schema, File fr) throws IOException {
-//        GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
-//        DataFileReader<GenericRecord> reader = new DataFileReader<>(fr, datumReader);
-//        while(reader.hasNext()){
-//            System.out.println(reader.next());
-//        }
-//        reader.close();
-//    }
-//
-//    public static void main(String[] args) throws IOException {
-//        Schema.Parser parser = new Schema.Parser();
-//        Schema schema = parser.parse(new File("src/main/resources/Employee.avsc"));
-//        GenericRecord[] employee = new GenericRecord[2];
-//        employee[0] = CreateEmployee(schema,1,"Nguyen","Lam",2001, 2.1,"CEO", "Management");
-//        employee[1] = CreateEmployee(schema,2,"Tran", "Ha",1999,1.7, "Staff","Sale");
-//        File fileSerialize = new File("employeeSerialize.avro");
-//        Serialize(schema, employee, fileSerialize);
-//        Deserialize(schema, fileSerialize);
-//    }
-//
-//}
+import java.io.File;
+import java.io.IOException;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.file.DataFileWriter;
+
+public class Program1 {
+    public static GenericRecord CreateEmployee(Schema schema, int id, String firstName, String lastName, int birthYear, double salaryRate, String position, String department){
+        GenericRecord employee = new GenericData.Record(schema);
+        employee.put("id", id);
+        employee.put("firstName", firstName);
+        employee.put("lastName", lastName);
+        employee.put("birthYear", birthYear);
+        employee.put("salaryRate", salaryRate);
+        employee.put("position", position);
+        employee.put("department", department);
+        return employee;
+    }
+
+    public static void Serialize(Schema schema, GenericRecord[] employee, File fw) throws IOException{
+        GenericDatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<> (schema);
+        DataFileWriter<GenericRecord> writer = new DataFileWriter<>(datumWriter);
+        writer.create(schema, fw);
+        for(GenericRecord i:employee){
+            writer.append(i);
+        }
+        writer.close();
+    }
+
+    public static void Deserialize(Schema schema, File fr) throws IOException {
+        GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+        DataFileReader<GenericRecord> reader = new DataFileReader<>(fr, datumReader);
+        while(reader.hasNext()){
+            System.out.println(reader.next());
+        }
+        reader.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(new File("src/main/resources/Employee.avsc"));
+        GenericRecord[] employee = new GenericRecord[2];
+        employee[0] = CreateEmployee(schema,1,"Nguyen","Lam",2001, 2.1,"CEO", "Management");
+        employee[1] = CreateEmployee(schema,2,"Tran", "Ha",1999,1.7, "Staff","Sale");
+        File fileSerialize = new File("employeeSerialize.avro");
+        Serialize(schema, employee, fileSerialize);
+        Deserialize(schema, fileSerialize);
+    }
+
+}
--- Task3/src/main/java/Program2.java
+++ Task3/src/main/java/Program2.java
@@ -1,40 +1,40 @@
-import com.example.*;
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.avro.specific.SpecificDatumWriter;
-
-import java.io.File;
-import java.io.IOException;
-
-public class Program2 {
-    public static void Serialize(Employee[] employee, File fileSerialize) throws IOException{
-        DatumWriter<Employee> datumWriter = new SpecificDatumWriter<>(Employee.class);
-        DataFileWriter<Employee> fileWriter = new DataFileWriter<>(datumWriter);
-        fileWriter.create(employee[0].getSchema(), fileSerialize);
-        for(Employee i:employee) {
-            fileWriter.append(i);
-        }
-        fileWriter.close();
-    }
-
-    public static void Deserialize(File fileSerialize) throws IOException{
-        DatumReader<Employee> datumReader = new SpecificDatumReader<>(Employee.class);
-        DataFileReader<Employee> fileReader = new DataFileReader<>(fileSerialize, datumReader);
-        while(fileReader.hasNext()){
-            System.out.println(fileReader.next());
-        }
-        fileReader.close();
-    }
-
-    public static void main(String[] args) throws IOException {
-        Employee[] employee = new Employee[2];
-        employee[0] = new Employee(1,"Nguyen","Lam",2001, 2.1,"CEO", department.Management);
-        employee[1] = new Employee(2,"Tran", "Ha",1999,1.7, "Staff",department.Sale);
-        File fileSerialize = new File("src/main/resources/employeeSerialize.avro");
-        Serialize(employee, fileSerialize);
-        Deserialize(fileSerialize);
-    }
-}
+//import com.example.*;
+//import org.apache.avro.file.DataFileReader;
+//import org.apache.avro.file.DataFileWriter;
+//import org.apache.avro.io.DatumReader;
+//import org.apache.avro.io.DatumWriter;
+//import org.apache.avro.specific.SpecificDatumReader;
+//import org.apache.avro.specific.SpecificDatumWriter;
+//
+//import java.io.File;
+//import java.io.IOException;
+//
+//public class Program2 {
+//    public static void Serialize(Employee[] employee, File fileSerialize) throws IOException{
+//        DatumWriter<Employee> datumWriter = new SpecificDatumWriter<>(Employee.class);
+//        DataFileWriter<Employee> fileWriter = new DataFileWriter<>(datumWriter);
+//        fileWriter.create(employee[0].getSchema(), fileSerialize);
+//        for(Employee i:employee) {
+//            fileWriter.append(i);
+//        }
+//        fileWriter.close();
+//    }
+//
+//    public static void Deserialize(File fileSerialize) throws IOException{
+//        DatumReader<Employee> datumReader = new SpecificDatumReader<>(Employee.class);
+//        DataFileReader<Employee> fileReader = new DataFileReader<>(fileSerialize, datumReader);
+//        while(fileReader.hasNext()){
+//            System.out.println(fileReader.next());
+//        }
+//        fileReader.close();
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//        Employee[] employee = new Employee[2];
+//        employee[0] = new Employee(1,"Nguyen","Lam",2001, 2.1,"CEO", department.Management);
+//        employee[1] = new Employee(2,"Tran", "Ha",1999,1.7, "Staff",department.Sale);
+//        File fileSerialize = new File("src/main/resources/employeeSerialize.avro");
+//        Serialize(employee, fileSerialize);
+//        Deserialize(fileSerialize);
+//    }
+//}
